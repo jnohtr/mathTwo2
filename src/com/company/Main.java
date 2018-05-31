86f14c13295ed62e574e086acc0ff441cb14142c
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 15:28:36 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int mynumber = 2;
+        double myothernumber = 1.7934;
+        System.out.println (mynumber);
+        System.out.print (myothernumber);
+    }
+}
